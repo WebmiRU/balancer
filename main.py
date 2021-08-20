5cd78ad5be6f463f35432520350637f1df2d7dfc
E.Wolf <e.wolf@webmi.ru>
2021-08-20 15:22:28 +0300

    Работает отправка ответов за запросы из клиента (браузера)

--- main.py
+++ main.py
@@ -1,36 +1,59 @@
 import asyncio
+import json
 from pprint import pprint
 import time
 import websockets
+import docker
 
 
 class Server:
     ws_clients = {}
+    containers = {}
+    client = docker.from_env()
 
-    async def handler(self, ws, path):
+    def __init__(self):
+        for container in self.client.containers.list(all=True):
+            self.container_set(container)
+
+    def container_set(self, container):
+        key = container.attrs['Id']
+        value = {
+            'name': container.name,
+            'attrs': container.attrs,
+            'labels': container.labels
+        }
+
+        self.containers[key] = value
+
+    def docker_events(self):
+        for event in self.client.events(decode=True):
+            if event.get('id'):
+                container = self.client.containers.get(event['id'])
+                self.container_set(container)
+                self.ws_client_notify()
+                print('UPDATED!')
+
+    async def ws_client_notify(self):
+        for ws in self.ws_clients:
+            for k, v in self.containers.items():
+                await ws.send(json.dumps({'Name': v['name'], 'State': v['attrs']['State']['Running']}))
+
+    async def ws_handler(self, ws, path):
         self.ws_clients[ws] = ws
 
         try:
             async for request in ws:
+                await self.ws_client_notify()
                 print("MESSAGE: " + request)
-                await ws.send("Hello world!")
         except:
             self.ws_clients.pop(ws)
 
-    def f1(self):
-        while True:
-            print('F1')
-            time.sleep(1)
-
-    def f2(self):
-        while True:
-            print('COUNT: %s' % len(self.ws_clients))
-            time.sleep(1)
-
     async def run(self):
-        await websockets.serve(self.handler, "localhost", 6789)
-        await asyncio.gather(asyncio.to_thread(self.f1), asyncio.to_thread(self.f2))
+        await websockets.serve(self.ws_handler, '0.0.0.0', 6789)
+        # await self.docker_events()
+        await asyncio.gather(asyncio.to_thread(self.docker_events))
 
 
 server = Server()
 asyncio.run(server.run())
+# asyncio.get_event_loop().run_until_complete(server.run())
